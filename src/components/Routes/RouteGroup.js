2854d373d122c10d6da878dd142462e4d5a5efca
James LeClair <james@pixelpro.dev>
2019-11-21 12:04:25 -0500

    make route path a link

--- src/components/Routes/RouteGroup.js
+++ src/components/Routes/RouteGroup.js
@@ -12,12 +12,17 @@ const Path = styled.div({
   fontSize: 16,
   fontWeight: 600,
   padding: '15px 30px',
-  borderBottom: '1px solid #DFE2E5'
+  borderBottom: '1px solid #DFE2E5',
+  ' a': {
+    color: '#444'
+  }
 })
 export default function RouteGroup ({ path, routes }) {
   return (
     <Container>
-      <Path>{path}</Path>
+      <Path>
+        <a href={path}>{path}</a>
+      </Path>
       <ModifierHeader />
       { routes.map(r => <Route key={r.id} {...r} />) }
     </Container>

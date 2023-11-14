7a545e97ea5bfd333757dc35fa5669cb4ff6fff4
James LeClair <james@pixelpro.dev>
2023-11-13 23:32:57 -0500

    Upgrade to uplifted strap and align major versions to match (#30)

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const Appstrap = require('../appstrap')
+const Appstrap = require('@appstrap/core')
 
 module.exports = {
   entry: {
